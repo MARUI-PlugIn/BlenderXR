de952d5b89289b8409dc955918ae16d2de807ca4
Peter Kim <pk15950@gmail.com>
2018-10-15 12:40:58 +0900

    Added pre-compiled binaries (tracked with git lfs) and updated a dead link for building linux static dependencies.

--- blender/build_files/build_environment/cmake/versions.cmake
+++ blender/build_files/build_environment/cmake/versions.cmake
@@ -21,8 +21,8 @@ set(ZLIB_URI https://zlib.net/zlib-${ZLIB_VERSION}.tar.gz)
 set(ZLIB_HASH 1c9f62f0778697a09d36121ead88e08e)
 
 set(OPENAL_VERSION 1.18.2)
-set(OPENAL_URI http://kcat.strangesoft.net/openal-releases/openal-soft-${OPENAL_VERSION}.tar.bz2)
-set(OPENAL_HASH d4eeb0889812e2fdeaa1843523d76190)
+set(OPENAL_URI https://github.com/kcat/openal-soft/archive/openal-soft-${OPENAL_VERSION}.tar.gz)
+set(OPENAL_HASH fa2cb3df766ab5976c86efbcc1d24d68)
 
 set(PNG_VERSION 1.6.35)
 set(PNG_URI http://prdownloads.sourceforge.net/libpng/libpng-${PNG_VERSION}.tar.xz)
